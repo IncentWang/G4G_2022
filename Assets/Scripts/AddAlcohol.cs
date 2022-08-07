8adeafa967734659fb1b596eaeeaab5350c0f0d2
‘Wymen’ <liweimin0406@163.com>
2022-08-07 12:28:59 +0800

    修改调酒游戏slider

--- Assets/Scripts/AddAlcohol.cs
+++ Assets/Scripts/AddAlcohol.cs
@@ -5,7 +5,12 @@ using UnityEngine.UI;
 
 public class AddAlcohol : MonoBehaviour
 {
+    bool buttonDown;
     bool addAlcohol;
+    public float switchSpeedTime;
+    float speed=1;
+
+    private NoRepeatRandom nrr;
     public Slider slider;
     // Start is called before the first frame update
     void Start()
@@ -16,15 +21,33 @@ public class AddAlcohol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (addAlcohol)
+        switchSpeedTime+=Time.deltaTime;
+        if (switchSpeedTime >= 1)
         {
-            slider.value += Time.deltaTime;
+            nrr = new NoRepeatRandom(1000);
+            speed = nrr.Next() * 2.5f;
+            switchSpeedTime = 0;
+        }
+        
+        if (buttonDown && addAlcohol)
+        {
+            slider.value += Time.deltaTime* speed;
+        }else if (buttonDown && !addAlcohol)
+        {
+            slider.value -= Time.deltaTime* speed;
+        }
+        if (slider.value == 1)
+        {
+            addAlcohol = false;
+        }else if (slider.value == 0)
+        {
+            addAlcohol= true;
         }
     }
     
     public void OnClickAddAlcoholButton(bool down)
     {
-        addAlcohol = down;
+        buttonDown = down;
         if (down)
         {
             slider.value = 0;
