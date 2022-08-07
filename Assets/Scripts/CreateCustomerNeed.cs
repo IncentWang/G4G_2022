91511568ac19689d6552e725c5916676c49645af
‘Wymen’ <liweimin0406@163.com>
2022-08-07 10:02:49 +0800

    根据权重随机生成顾客需求

---
+++ Assets/Scripts/CreateCustomerNeed.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DefaultNamespace;
+using UnityEngine.UI;
+
+public class CreateCustomerNeed : MonoBehaviour
+{
+    private NoRepeatRandom nrr;
+    public  ExcelDataManager dataManager;
+
+    public Text sweet;
+    public Text intensity;
+    public Text mellow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetChild(0).GetChild(0).TryGetComponent<Text>(out sweet);
+        transform.GetChild(1).GetChild(0).TryGetComponent<Text>(out intensity);
+        transform.GetChild(2).GetChild(0).TryGetComponent<Text>(out mellow);
+        nrr = new NoRepeatRandom(1000);
+        var rdm=nrr.Next() * 100;
+        Debug.Log(rdm);
+        var weightSum=0;
+        for(int i=0;i<dataManager.Need.Length-1;i++)
+        {
+            weightSum += dataManager.Need[i].weight;
+            if (weightSum > rdm)
+            {
+                //根据权重随机出顾客需求
+                Debug.Log(dataManager.Need[i-1].id);
+                sweet.text = NeedToString(dataManager.Need[i - 1].needSweet);
+                intensity.text = NeedToString(dataManager.Need[i - 1].needIntensity);
+                mellow.text = NeedToString(dataManager.Need[i - 1].needMellow);
+                return;
+            }
+
+        }
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private string NeedToString(int need)
+    {
+        if (need <= 5)
+        {
+            return "轻微";
+        }else if (need > 5 && need <= 10)
+        {
+            return "适中";
+        }
+        else
+        {
+            return "很浓";
+        }
+    }
+}
