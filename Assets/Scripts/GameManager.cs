36ffc0171df138ec608dd339e9efa12df8a9a870
IncentWang <wwhincent@gmail.com>
2022-08-07 12:41:58 +0800

    添加了空杯材质、为需求泡泡的显示添加了API。

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 使用该类来放置游戏中的所有游戏时数据、Flowchart、工具函数
+    public GameObject NeedBubble;
+
+    public void ShowNeedBubble()
+    {
+        NeedBubble.SetActive(true);
+    }
+
+    public void HideNeedBubble()
+    {
+        NeedBubble.SetActive(false);
+    }
+
+    public void Update()
+    {
+        // 仅用于测试API
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ShowNeedBubble();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            HideNeedBubble();
+        }
+    }
+}
