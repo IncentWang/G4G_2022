7002d7d26ee1a19a7ca54c5cc5ad8746d676f79b
‘Wymen’ <liweimin0406@163.com>
2022-08-08 13:56:14 +0800

    飞镖游戏加ai

--- Assets/Scripts/ShootGamePlayerController.cs
+++ Assets/Scripts/ShootGamePlayerController.cs
@@ -7,73 +7,93 @@ namespace DefaultNamespace
     public class ShootGamePlayerController : MonoBehaviour
     {
         public RectTransform DotPosition;
-        public DartsMove Dart;
-        public Text scoreText;
+        public DartsMove playerDart;
+        public Text playerScoreText;
         public List<AudioClip> clips;
         public Transform cam;
+
+        public float time;
+        public DartsMove Dart;
+        public Text scoreText;
         private void Start()
         {
             GameObject.Find("Main Camera").TryGetComponent<Transform>(out cam);
         }
+        private void OnEnable()
+        {
+            System.Random random = new System.Random();
+            time = random.Next(3, 5);
+        }
         private void Update()
         {
+            time -= Time.deltaTime;
+            if (time < 0)
+            {
+                shoot(Dart, scoreText);
+            }
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                AudioSource.PlayClipAtPoint(clips[0],cam.position);
-                AudioSource.PlayClipAtPoint(clips[1],cam.position);
-                // 开始执行射线检测
-                RaycastHit2D[] hits = Physics2D.RaycastAll(DotPosition.anchoredPosition, Vector2.zero);
-                Ray ray = new Ray(Camera.main.transform.position,
-                    DotPosition.position - Camera.main.transform.position);
-                RaycastHit2D[] hit2s = Physics2D.GetRayIntersectionAll(ray);
-                Debug.Log(hit2s.Length);
-                Dart.speed = 0;
-                switch (hit2s.Length)
-                {
-                    case 2:
-                        Debug.Log("1 point");
-                        scoreText.text="1".ToString();
-                        break;
-                    case 3:
-                        Debug.Log("2 point");
-                        scoreText.text = "2".ToString();
-                        break;
-                    case 4:
-                        Debug.Log("3 point");
-                        scoreText.text = "3".ToString();
-                        break;
-                    case 5:
-                        Debug.Log("4 point");
-                        scoreText.text = "4".ToString();
-                        break;
-                    case 6:
-                        Debug.Log("5 point");
-                        scoreText.text = "5".ToString();
-                        break;
-                    case 7:
-                        Debug.Log("6 point");
-                        scoreText.text = "6".ToString();
-                        break;
-                    case 8:
-                        Debug.Log("7 point");
-                        scoreText.text = "7".ToString();
-                        break;
-                    case 9:
-                        Debug.Log("8 point");
-                        scoreText.text = "8".ToString();
-                        break;
-                    case 10:
-                        Debug.Log("9 point");
-                        scoreText.text = "9".ToString();
-                        break;
-                    case 11:
-                        Debug.Log("10 point");
-                        scoreText.text = "10".ToString();
-                        break;
-                    default:
-                        break;
-                }
+                AudioSource.PlayClipAtPoint(clips[0], cam.position);
+                AudioSource.PlayClipAtPoint(clips[1], cam.position);
+                shoot(playerDart, playerScoreText);
                 UIManager.Instance.PopPanel();
+            }
+        }
+        void shoot(DartsMove Dart, Text scoreText)
+        {
+            AudioSource.PlayClipAtPoint(clips[0], cam.position);
+            AudioSource.PlayClipAtPoint(clips[1], cam.position);
+            // 开始执行射线检测
+            RaycastHit2D[] hits = Physics2D.RaycastAll(DotPosition.anchoredPosition, Vector2.zero);
+            Ray ray = new Ray(Camera.main.transform.position,
+                DotPosition.position - Camera.main.transform.position);
+            RaycastHit2D[] hit2s = Physics2D.GetRayIntersectionAll(ray);
+            Debug.Log(hit2s.Length);
+            Dart.speed = 0;
+            switch (hit2s.Length)
+            {
+                case 2:
+                    Debug.Log("1 point");
+                    scoreText.text = "1".ToString();
+                    break;
+                case 3:
+                    Debug.Log("2 point");
+                    scoreText.text = "2".ToString();
+                    break;
+                case 4:
+                    Debug.Log("3 point");
+                    scoreText.text = "3".ToString();
+                    break;
+                case 5:
+                    Debug.Log("4 point");
+                    scoreText.text = "4".ToString();
+                    break;
+                case 6:
+                    Debug.Log("5 point");
+                    scoreText.text = "5".ToString();
+                    break;
+                case 7:
+                    Debug.Log("6 point");
+                    scoreText.text = "6".ToString();
+                    break;
+                case 8:
+                    Debug.Log("7 point");
+                    scoreText.text = "7".ToString();
+                    break;
+                case 9:
+                    Debug.Log("8 point");
+                    scoreText.text = "8".ToString();
+                    break;
+                case 10:
+                    Debug.Log("9 point");
+                    scoreText.text = "9".ToString();
+                    break;
+                case 11:
+                    Debug.Log("10 point");
+                    scoreText.text = "10".ToString();
+                    break;
+                default:
+                    break;
             }
         }
     }
