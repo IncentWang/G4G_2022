e5a6359598b6da5494905044f24683693b2e514e
IncentWang <wwhincent@gmail.com>
2022-08-08 15:59:29 +0800

    剧情已经添加完整。

---
+++ Assets/Scripts/CameraFader.cs
@@ -0,0 +1,12 @@
+﻿using Fungus;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraFader : MonoBehaviour
+{
+    public Image Image;
+    public void FadeOut(float duration)
+    {
+        iTween.FadeTo(Image.gameObject, 0, duration);
+    }
+}
